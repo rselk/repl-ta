06ff1ce25beceb0cbedfc6aa9cf3bf46a6bf54f0
Ryan Selk <ryanselk@gmail.com>
2018-08-26 13:41:09 -0600

    Add HomePage and LoginPage
    - Add rememberme button
    - All actions are mocked out for now

---
+++ client/src/HomePage/HomePage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(userActions.getAll());
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hello {user.firstName} {user.lastName}</h1>
+
+                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return {
+        user,
+        users
+    };
+}
+
+const connectedHomePage = connect(mapStateToProps)(HomePage);
+export { connectedHomePage as HomePage };
